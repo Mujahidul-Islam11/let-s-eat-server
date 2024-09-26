578fdcfaef1426e555ccf340860f35abd5c1cc5f
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-26 18:15:01 +0600

    Loaded data from the menu collection

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u8ojnwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -25,14 +24,22 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    // database collections
+    const database = client.db("LetsEatDB");
+    const menuCollection = database.collection("menu");
+
+    // crud operations
+    app.get("/menu", async(req, res)=> {
+        const result = await menuCollection.find().toArray();
+        res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
