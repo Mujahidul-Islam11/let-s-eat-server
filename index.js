fdd094b515ceae89e1bb17f40d38935f3ac2655d
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-10-21 15:46:50 +0600

    Created operation to update user data

--- index.js
+++ index.js
@@ -75,6 +75,18 @@ async function run() {
       res.send(result);
     })
 
+    app.patch("/users/:id", async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: "Admin"
+        }
+      }
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
